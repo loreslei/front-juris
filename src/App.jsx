c6d32ffbd4d861ceb14c40506ca963b8765c81d3
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2026-04-16 16:40:29 -0300

    renamed page 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Intro from "./Pages/Intro";
 import Process from "./Pages/Process";
-import NotFound from "./Pages/NotFound";
+import NotFound from "./Pages/404";
 
 function App() {
   return (
